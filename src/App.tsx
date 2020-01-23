d233d4dcea092cc7c086ba87aa2e91a3d3a7ddd4
Lixquid <lixquid@waifu.club>
2020-01-23 00:58:50 +0000

    Add color palette functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,144 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo, useRef, useState } from "react";
+import data from "./colorpalettes.json";
+import { format } from "./typescript-modules/build/StringUtilities";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IColorPickerAppProps {
+    onChange?: (output: string) => void;
 }
 
-export default App;
+const formats = {
+    Hex: "#${r:x2}${g:x2}${b:x2}",
+    RGB: "rgb(${r:d0}, ${g:d0}, ${b:d0})",
+    RGBA: "rgba(${r:d0}, ${g:d0}, ${b:d0}, 1)"
+};
+
+const ColorPickerApp: React.FC = () => {
+    const outputEl: React.MutableRefObject<HTMLInputElement | null> = useRef(null);
+    const [selectedPalette, setSelectedPalette] = useState(Object.keys(data)[0]);
+    const [selectedColor, setSelectedColor] = useState("");
+    const [selectedFormat, setSelectedFormat] = useState("Hex");
+    const [customFormat, setCustomFormat] = useState("");
+
+    const output = useMemo(() => {
+        if (!selectedColor) {
+            return "";
+        }
+        const color = {
+            r: parseInt(selectedColor.substring(0, 2), 16),
+            g: parseInt(selectedColor.substring(2, 4), 16),
+            b: parseInt(selectedColor.substring(4, 6), 16)
+        };
+        return format(
+            selectedFormat === "Custom" ? customFormat : formats[selectedFormat as keyof typeof formats],
+            color
+        );
+    }, [selectedColor, selectedFormat, customFormat]);
+
+    function setOutputToClipboard() {
+        const el = outputEl.current;
+        if (!el) {
+            return;
+        }
+        el.focus();
+        el.select();
+        document.execCommand("copy");
+    }
+
+    return (
+        <>
+            <h1>
+                Color Picker
+                <a href="https://lixquid.com" className="btn btn-outline-primary float-right">
+                    lixquid.com
+                </a>
+            </h1>
+
+            <div className="input-group input-group-lg mt-5">
+                <input
+                    type="text"
+                    name="output"
+                    id="output"
+                    className="form-control"
+                    value={output}
+                    ref={outputEl}
+                    readOnly
+                />
+                <div className="input-group-append">
+                    <button
+                        className="btn btn-outline-secondary dropdown-toggle"
+                        type="button"
+                        data-toggle="dropdown"
+                        aria-haspopup="true"
+                        aria-expanded="false"
+                    >
+                        {selectedFormat}
+                    </button>
+                    <div className="dropdown-menu">
+                        {Object.keys(formats).map(format => (
+                            <a
+                                className="dropdown-item"
+                                href="#"
+                                onClick={e => {
+                                    e.preventDefault();
+                                    setSelectedFormat(format);
+                                }}
+                            >
+                                {format}
+                            </a>
+                        ))}
+                        <input
+                            type="text"
+                            className="dropdown-item form-control"
+                            placeholder="Custom"
+                            value={customFormat}
+                            onChange={e => setCustomFormat(e.target.value)}
+                            onFocus={() => setSelectedFormat("Custom")}
+                        />
+                    </div>
+                </div>
+                <div className="input-group-append">
+                    <button className="btn btn-success" onClick={() => outputEl.current && setOutputToClipboard()}>
+                        Copy
+                    </button>
+                </div>
+            </div>
+
+            <div className="card mt-3">
+                <div className="card-body">
+                    <select className="custom-select">
+                        {Object.keys(data).map(name => (
+                            <option
+                                selected={selectedPalette === name}
+                                onClick={e => {
+                                    e.preventDefault();
+                                    setSelectedPalette(name);
+                                }}
+                            >
+                                {name}
+                            </option>
+                        ))}
+                    </select>
+                    {Object.keys(data)
+                        .filter(name => name === selectedPalette)
+                        .map(name => (
+                            <div className="px-4 pt-2">
+                                {data[name as keyof typeof data].map(cells => (
+                                    <div className="row">
+                                        {cells.map(color => (
+                                            <button
+                                                className="btn btn-secondary col m-1"
+                                                style={{ backgroundColor: `#${color}`, height: "2rem" }}
+                                                onClick={() => setSelectedColor(color)}
+                                            />
+                                        ))}
+                                    </div>
+                                ))}
+                            </div>
+                        ))}
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default ColorPickerApp;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+import React from "react";
+import ReactDOM from "react-dom";
+import ColorPickerApp from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+    <div className="mx-auto my-5 container">
+        <ColorPickerApp />
+    </div>,
+    document.getElementById("root")
+);
+serviceWorker.register();
