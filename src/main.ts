132ba55a5b112d7a961d22c9614576ac248fe1fc
Lixquid <lixquid@lixquid.com>
2020-03-06 00:45:56 +0000

    Add initial layout

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
+import VueCompositionApi from "@vue/composition-api";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
 
+Vue.use(VueCompositionApi);
 new Vue({
-  render: h => h(App)
+    render: h => h(App)
 }).$mount("#app");
