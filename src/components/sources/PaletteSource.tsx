fae6ca1d07c7cc1b46a2e189cfe888f715cc8bc2
Lixquid <lixquid@lixquid.com>
2023-05-08 22:34:26 +0100

    Add Palette Source remembering what palette is selected for the session

--- src/components/sources/PaletteSource.tsx
+++ src/components/sources/PaletteSource.tsx
@@ -5,16 +5,25 @@ import { ColorSourceProps } from "./sources";
 
 /** Provides a list of colors drawn from a JSON file. */
 export function PaletteSource(props: ColorSourceProps) {
-    const [palette, setPalette] = useState<keyof typeof palettes>("Oxygen");
+    const [palette, setPalette] = useState<keyof typeof palettes>(() => {
+        // Get from session storage, otherwise default to "Oxygen"
+        return (
+            (sessionStorage.getItem("palette") as keyof typeof palettes) ??
+            "Oxygen"
+        );
+    });
 
     return (
         <div class="card-body">
             <select
                 class="form-select mb-3"
                 value={palette}
-                onChange={(e) =>
-                    setPalette(e.currentTarget.value as keyof typeof palettes)
-                }
+                onChange={(e) => {
+                    sessionStorage.setItem("palette", e.currentTarget.value);
+                    return setPalette(
+                        e.currentTarget.value as keyof typeof palettes
+                    );
+                }}
             >
                 {Object.keys(palettes).map((palette, i) => (
                     <option key={i}>{palette}</option>
